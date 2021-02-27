54d905b0e66ba46f6ea1ddb1647d2de3b3b3fe73
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2021-02-27 11:44:49 -0300

    Started layout implementation
    Started outlining the main page layout for posts using a flexbox grid.

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <?php get_header(); ?>
 
-<div>
-  <?php
-    if (have_posts()) {
-      while(have_posts()){
-        the_post();
-        ?><a href="<?php the_permalink()?>"><?php the_post_thumbnail(); ?></a><?php
+<div class="main-window">
+  <div class="post-board">
+    <?php
+      if (have_posts()) {
+        while(have_posts()){
+          the_post();
+          ?> <!-- the post index content goes in here -->
+            <div class="post-index">
+              <a class="post-index" href="<?php the_permalink(); ?>">
+                <img class="post-index" src="<?php the_post_thumbnail(); ?>"/>
+                <p><?php the_post_title(); ?></p>
+              </a>
+            </div>
+          <?php
+        }
+      } else {
+        ?><p>Sorry, no posts found.</p><?php
       }
-    } else {
-      ?><p>Sorry, no posts found.</p><?php
-    }
-  ?>
+    ?>
+  </div>
 </div>
 
 <?php get_footer() ?>
