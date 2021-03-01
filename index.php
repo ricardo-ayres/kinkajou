da72f6014b6973b70435045ce5e4a044cdcf64f1
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2021-02-28 21:36:53 -0300

    works with inline style. now on to fix the css.

--- index.php
+++ index.php
@@ -1,27 +1,49 @@
 <!DOCTYPE html>
 <html>
   <head><?php wp_head(); ?></head>
-  <body>
 
-    <!-- sidenav -->
-    <div class="sidenav external">
-      <div class="sidenav internal">
-        <a>a link</a>
+<body style="display: block;
+             height: 100vh;
+             overflow-y: hidden;">
+
+  <!-- side nav -->
+  <div style="display: block;
+              float: left;
+              height:100%;
+              width: 15%;
+              overflow-y: hidden;">
+    <div style="width: calc(100% + 15px);
+                height: 100%;
+                overflow-y: scroll;">
+      <a style="display: block;">a link in the sidenav</a>
+    </div>
+  </div>
+
+  <!-- main post board -->
+  <div style="display: flex;
+              flex-flow: column wrap;
+              float: left;
+              height: 100%;">
+    <?php while (have_posts()): ?>
+      <?php the_post(); ?>
+      <div style="display: block;
+                  height: 30%;
+                  padding: 1%;">
+        <a style="display: block;
+                  height: 100%;
+                  width: 100%;"
+          href="<?php the_permalink(); ?>">
+          <img style="display: block;
+                      margin: auto;
+                      max-width: 100%;
+                      max-height: 100%;"
+            src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>"/>
+        </a>
       </div>
-    </div>
+    <?php endwhile ?>
+  </div>
 
-    <!-- main post index -->
-    <div class="postboard">
-      <?php while (have_posts()): ?>
-        <?php the_post(); ?>
-        <div class="postboard postitem">
-          <a style="display: block; height: 100%; width: 100%;" href="<?php the_permalink(); ?>">
-            <img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>" style="margin: auto;"/>
-          </a>
-        </div>
-      <?php endwhile ?>
-    </div>
 
-    <footer><?php wp_footer(); ?></footer>
-  </body>
+  <footer><?php wp_footer(); ?></footer>
+</body>
 </html>
