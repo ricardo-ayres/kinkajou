e58674b2ba5756255907bf4077593e73ceaa6bd9
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2021-03-25 23:08:53 -0300

    formatting page output

--- page.php
+++ page.php
@@ -1,14 +1,19 @@
 <?php get_header(); ?>
 
-<div id="main-content" class="main-content">
+<div id="main-content" class="page content container padding">
   <?php
     /* the loop */
     if ( have_posts() ) {
       while ( have_posts() ) {
         /* Set up post data */
         the_post();
+
+        /* Post title */
+        ?>
+          <h1 class="page content title"><?php the_title(); ?></h1>
+        <?php
+
         /* Output content */
-        the_title();
         the_content();
       }
     } else {
