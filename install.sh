ad5486e801c04d71d61205bbc977a1f58fcd5c41
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2021-04-06 19:26:20 -0300

    adjusting install script

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 WP_THEMES=/var/www/e00m/wp-content/themes
 
-mkdir -pv $WP_THEMES/kinkajou
-cp -rv * $WP_THEMES/kinkajou/
+case $@ in
+    *remote*)
+        pushd ..
+        rsync -rv kinkajou e00m.duckdns.org:$WP_THEMES
+        popd
+    ;;
+    *)
+        mkdir -pv $WP_THEMES/kinkajou
+        cp -rv * $WP_THEMES/kinkajou/
+    ;;
+esac
